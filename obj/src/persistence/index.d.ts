5d7315563248e30acc873b8047d3fb4d40f88e00
Denis Kuznetsov <deniskuzn@gmail.com>
2020-08-05 17:41:00 +0300

    fixed exports

--- obj/src/persistence/index.d.ts
+++ obj/src/persistence/index.d.ts
@@ -2,3 +2,7 @@ export { IReviewsPersistence } from './IReviewsPersistence';
 export { ReviewsMemoryPersistence } from './ReviewsMemoryPersistence';
 export { ReviewsFilePersistence } from './ReviewsFilePersistence';
 export { ReviewsMongoDbPersistence } from './ReviewsMongoDbPersistence';
+export { IRatingsPersistence } from './IRatingsPersistence';
+export { RatingsMemoryPersistence } from './RatingsMemoryPersistence';
+export { RatingsFilePersistence } from './RatingsFilePersistence';
+export { RatingsMongoDbPersistence } from './RatingsMongoDbPersistence';
